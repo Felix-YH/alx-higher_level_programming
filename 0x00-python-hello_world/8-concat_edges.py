b68fff9531cfd9470443dd202513d36392943692
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-05-30 15:21:05 +0100

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-       language that combines remarkable power with clear syntax"
-str = str[39:67]  + str[-17:-13] + str [0:7]
+ language that combines remarkable power with clear syntax"
+str = str[39:67]  + str[107:112] + str [:6]
 print(str)
