b9c9675a6837a7acdbb65471ff1b2adf2dd7779f
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-06-08 18:09:17 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_d = a_dictionary.copy()
-    for key in sorted(new_d):
-        print("{}: {}".format(key,new_d[key] * 2))
+    new_dic = {}
+    for key in a_dictionary:
+        new_dic[keey] = a_dictionary[key] * 2
+    return new_dic
