4c20a57751efac48e00d7e413b7e48a7af208f5a
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-06-08 12:48:37 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,6 @@
-
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for x in range(len(my_list)):
+        if new_list[x] == search:
+            new_list[x] = replace
+    return new_list
