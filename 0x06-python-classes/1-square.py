31a497f9ff757079d8bf257718afccc917d1fc44
fellydyll <cobbyswag@gmail.com>
2022-06-21 14:21:02 +0000

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Class that contains a square"""
 
+
 class Square:
     """ Represent a square with 
     private attribute"""
     def __init__(self, size):
-        """Initializes a square.
-        Args:
-            size: size of a square
-        """
+    """Initializes a square.
+    Args:
+        size: size of a square
+    """
     self.__size = size
