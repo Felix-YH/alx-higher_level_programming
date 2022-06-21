abc169f735e49f5d584c876d4aa14941343b626c
fellydyll <cobbyswag@gmail.com>
2022-06-21 15:34:31 +0000

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """ Represent a square with public instance
+    with getter and setter
+    """
+
+    def __init__(self, size=0):
+        """Initialise a sqaure
+        Args:
+            size = size of square
+        """
+        self.__size
+
+    @property
+    def size(self):
+        """ Retrives size of a square"""
+        return self.size
+
+    @size.setter
+    def size(self, value):
+        """ sets the size of a sqaure
+        Args:
+        Value: size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the current area"""
+        return (self.__size ** 2)
