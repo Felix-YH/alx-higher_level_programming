3b98776e3f613d17ff11c7bf380561991c803151
fellydyll <cobbyswag@gmail.com>
2022-06-21 14:13:05 +0000

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Defines a square with private instance attribute"""
+
+class Square:
+    """ Represent a square with size(attribute)"""
+    def __init__(self, size):
+    self.__size = size
