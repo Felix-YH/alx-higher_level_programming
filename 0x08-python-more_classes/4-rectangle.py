b8110212a7077def20ada3a9cfaa2171fc12cbd2
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-06-27 15:57:17 +0000

    Update 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -75,4 +75,4 @@ class Rectangle:
         """return a string representation of the rectangle/
         to be able to recreate a new instance by using eval()
         """
-        return ("Rectangle({}, {})".format(self.__width, self.height))
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
