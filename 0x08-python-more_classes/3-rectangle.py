4501860ca99054eddaa46f706708b4ffa269c773
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-06-27 15:23:34 +0000

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle:
 
     def __str__(self):
         """print # in form of a rectangle"""
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ("")
         rec_gle = ""
         for i in range(self.__width):
