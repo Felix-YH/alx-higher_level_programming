f4e904cd26838766edfae7d7f7de9613ee818279
fellydyll <cobbyswag@gmail.com>
2022-07-05 16:45:27 +0000

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,10 +9,10 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """"Retrieves a dictionary representation of a
         Student instance.Returns:the dict representation
         of the instance
         """
         return self.__dict__
-
