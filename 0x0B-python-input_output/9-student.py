a99c9646c077fcc6ba77138df63faa7fcaea99a9
fellydyll <cobbyswag@gmail.com>
2022-07-05 16:43:37 +0000

    create 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This write a class Student that defines a student"""
+
+
+class Student:
+    """class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """"Retrieves a dictionary representation of a
+        Student instance.Returns:the dict representation
+        of the instance
+        """
+        return self.__dict__
+
