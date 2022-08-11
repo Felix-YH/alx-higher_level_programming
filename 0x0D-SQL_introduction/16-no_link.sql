dd6e99e8760f6876773256bbec754ee970ce0173
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-08-11 07:16:22 +0000

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
--- Lists all records of the table "second_table having a value
---in descending order.
+-- Lists all records of the table second_table having a name value.
+-- Records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
